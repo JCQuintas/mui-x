002ae3e3a887f7ea8034a4002db297da79f63b8e
Jose Quintas <juniorquintas@gmail.com>
2024-05-10 11:28:40 +0200

    add examples

--- packages/x-charts/src/types/datasss.tsx
+++ packages/x-charts/src/types/datasss.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { ChartProps, Dataset } from './data';
 
 const multiDatasetWithId = [
@@ -33,30 +34,53 @@ const multiDataset = [
   ],
 ] as const;
 
-const Chart = (props: ChartProps<typeof multiDataset>) => {
-  return <></>;
-};
+const simpleDataset = [
+  [
+    { x: 1, y: 2 },
+    { x: 2, y: 3 },
+  ],
+] as const;
 
-const ChartId = (props: ChartProps<typeof multiDatasetWithId>) => {
-  return <></>;
-};
+function Chart(props: ChartProps<typeof multiDataset>) {
+  return <div>{JSON.stringify(props)}</div>;
+}
 
-const Render = () => {
+function ChartId(props: ChartProps<typeof multiDatasetWithId>) {
+  return <div>{JSON.stringify(props)}</div>;
+}
+
+function ChartSimple(props: ChartProps<typeof simpleDataset>) {
+  return <div>{JSON.stringify(props)}</div>;
+}
+
+export function Render() {
   return (
-    <>
+    <React.Fragment>
       <Chart
         dataset={multiDataset}
-        series={[{ datasetIndex: 0, seriesKey: 'x', label: 'Goals' }]}
+        series={[
+          { datasetIndex: 0, seriesKey: 0, label: 'right' },
+          { datasetIndex: 2, seriesKey: 'x', label: 'right' },
+          { datasetIndex: 0, seriesKey: 'x', label: 'wrong' },
+        ]}
       />
       <ChartId
         dataset={multiDatasetWithId}
         series={[
-          { datasetIndex: 'third', seriesKey: 'x', label: 'Goals' },
-          { datasetIndex: 0, seriesKey: 0, label: 'Goals' },
-          { datasetIndex: 'second', seriesKey: 'y', label: 'Goals' },
-          { datasetIndex: 1, seriesKey: 1, label: 'Goals' },
+          { datasetIndex: 'third', seriesKey: 'x', label: 'right' },
+          { datasetIndex: 0, seriesKey: 0, label: 'right' },
+          { datasetIndex: 1, seriesKey: 1, label: 'right' },
+          { datasetIndex: 'second', seriesKey: 'y', label: 'wrong' },
+          { datasetIndex: 1, seriesKey: 'y', label: 'wrong' },
         ]}
       />
-    </>
+      <ChartSimple
+        dataset={simpleDataset}
+        series={[
+          { seriesKey: 'x', label: 'right' },
+          { seriesKey: 0, label: 'wrong' },
+        ]}
+      />
+    </React.Fragment>
   );
-};
+}
